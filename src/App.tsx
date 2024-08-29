5cecd716e245cbb52756b53c60ffe1709950cf8a
Daniil <piecyberpie@gmail.com>
2024-08-29 22:59:44 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Header from "./components/Header/Header.tsx";
+import "./styles/index.scss";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+export const Header: React.FC = () => (
+  <ul className="header_list">
+    <li className="header_list__item">💡LO!</li>
+    <li className="header_list__item">reset</li>
+  </ul>
+);
+
+export default Header;
