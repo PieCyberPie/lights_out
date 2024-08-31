9cb59017273031395d4121e9cb8aaff24f31d537
Daniil <piecyberpie@gmail.com>
2024-08-31 11:01:29 +0300

    added some components, styles, cleaned README.md

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import Header from "./components/Header/Header.tsx";
 import "./styles/index.scss";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,10 @@
+import logo from "../../assets/icon/logo.svg";
+
 export const Header: React.FC = () => (
   <ul className="header_list">
-    <li className="header_list__item">💡LO!</li>
+    <li className="header_list__item">
+      <img src={logo} alt="l" />
+    </li>
     <li className="header_list__item">reset</li>
   </ul>
 );
