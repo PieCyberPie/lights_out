2c469e1b270982e77e87b4cf1369a2ea73ac6ef8
Daniil <piecyberpie@gmail.com>
2024-09-11 21:39:25 +0300

    added footer, cursor pointer to various elements

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,35 @@
-const Footer = () => <p className="text">hello</p>;
+import contacts from "../../contacts/contacts.json";
+import TelegramIcon from "../../assets/contacts/telegram.svg";
+import GithubIcon from "../../assets/contacts/github.svg";
+
+type IconMap = {
+  [key: string]: string;
+};
+
+const iconMap: IconMap = {
+  Telegram: TelegramIcon,
+  GitHub: GithubIcon,
+  // Add other mappings here as needed
+};
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      {contacts.map((contact) => {
+        const IconComponent = iconMap[contact.name];
+        return (
+          <a href={contact.link} className="contact-container">
+            <img src={IconComponent} alt={contact.name} className="icon" />
+            <p>{contact.name}</p>
+          </a>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Footer;
+
+{
+  /* <img src={IconComponent} alt={contact.name} className="icon" />; */
+}
